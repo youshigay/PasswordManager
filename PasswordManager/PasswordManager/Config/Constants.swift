dbe7170570071d29b943e3ba7e68835d37e7192c
yzg <youzhigang@archforce.com.cn>
2026-03-24 16:06:09 +0800

    feat: add app constants configuration

---
+++ PasswordManager/PasswordManager/Config/Constants.swift
@@ -0,0 +1,59 @@
+//
+//  Constants.swift
+//  PasswordManager
+//
+//  Created by michaels on 2026/3/24.
+//
+
+import Foundation
+
+enum Constants {
+    // MARK: - Storage
+    enum Storage {
+        static let appSupportPath = "Application Support/PasswordManager"
+        static let vaultFileName = "vault.json"
+        static let backupFileExtension = "bak"
+
+        static var vaultURL: URL {
+            let appSupport = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!
+            let folder = appSupport.appendingPathComponent(appSupportPath)
+            try? FileManager.default.createDirectory(at: folder, withIntermediateDirectories: true)
+            return folder.appendingPathComponent(vaultFileName)
+        }
+    }
+
+    // MARK: - Security
+    enum Security {
+        static let keychainService = "com.passwordmanager.vault"
+        static let keychainAccount = "encryptedDEK"
+        static let pbkdf2Iterations = 100_000
+        static let keyLength = 32 // 256 bits
+        static let saltLength = 16
+    }
+
+    // MARK: - Authentication
+    enum Auth {
+        static let minPasswordLength = 8
+        static let maxFailedAttempts = 5
+        static let lockoutDuration: TimeInterval = 300 // 5 minutes
+        static let delayAfter3Failures: TimeInterval = 3
+        static let delayAfter4Failures: TimeInterval = 15
+    }
+
+    // MARK: - Clipboard
+    enum Clipboard {
+        static let clearInterval: TimeInterval = 30
+    }
+
+    // MARK: - UI
+    enum UI {
+        static let popoverWidth: CGFloat = 360
+        static let rowHeight: CGFloat = 44
+        static let defaultIcon = "🔒"
+    }
+
+    // MARK: - Import/Export
+    enum ImportExport {
+        static let currentVersion = 1
+    }
+}
